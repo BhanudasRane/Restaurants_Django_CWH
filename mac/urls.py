26cb8af6fdc258cc827e7a233654e6c059405593
BhanudasRane <ranebhushan786@gmail.com>
2020-11-04 01:21:05 +0530

    Third Commit

--- mac/urls.py
+++ mac/urls.py
@@ -19,14 +19,12 @@ from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
-from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),    
     path('blog/',include('blog.urls')),
     path('shop/',include('shop.urls')),
     path('',views.index),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+     
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
