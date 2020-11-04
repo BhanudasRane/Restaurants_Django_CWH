6ff9cb61217d60a3e122ee09b8657301acfcd601
BhanudasRane <65908286+BhanudasRane@users.noreply.github.com>
2020-11-04 12:50:14 +0530

    Update urls.py

--- mac/urls.py
+++ mac/urls.py
@@ -27,7 +27,7 @@ urlpatterns = [
     path('blog/',include('blog.urls')),
     path('shop/',include('shop.urls')),
     path('',views.index),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
      
 ]+ static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
